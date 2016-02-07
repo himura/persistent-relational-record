3428fd2c60ca2bc00d24855734891fbcad3501f9
Takahiro HIMURA <taka@himura.jp>
2016-02-07 20:50:51 +0900

    Add hlint test

---
+++ tests/hlint.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Control.Monad
+import Language.Haskell.HLint
+import System.Environment
+import System.Exit
+
+main :: IO ()
+main = do
+  args <- getArgs
+  hints <- hlint $ ["src", "--cpp-define=HLINT", "--cpp-ansi", "--cpp-file=dist/build/autogen/cabal_macros.h"] ++ args
+  unless (null hints) exitFailure
