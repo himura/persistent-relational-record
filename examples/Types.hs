95620cbbe7f9b4480273d16ba6af97650fbde364
Takahiro HIMURA <taka@himura.jp>
2016-02-14 05:01:19 +0900

    fix examples (wipe unused instances)

--- examples/Types.hs
+++ examples/Types.hs
@@ -3,11 +3,7 @@
 
 module Types where
 
-import Data.Convertible
 import qualified Data.Text as T
-import Database.HDBC (SqlValue)
-import Database.HDBC.Record.Persistable () -- PersistableValue SqlValue instance from Convertible
-import Database.HDBC.Record.TH
 import Database.Persist
 import Database.Persist.Relational
 import Database.Persist.Sql
@@ -20,16 +16,8 @@ data ImageType = JPEG | PNG | BMP | GIF
 
 -- * instances for HRR
 
-instance Convertible ImageType Int where
-    safeConvert = return . fromEnum
-instance Convertible Int ImageType where
-    safeConvert = return . toEnum
-instance Convertible SqlValue ImageType where
-    safeConvert = convertVia (undefined :: Int)
-instance Convertible ImageType SqlValue where
-    safeConvert = convertVia (undefined :: Int)
 deriveNotNullType [t|ImageType|]
-derivePersistableInstanceFromValue [t|ImageType|]
+defineFromToSqlPersistValue [t|ImageType|]
 defineScalarDegree [t|ImageType|]
 instance ShowConstantTermsSQL ImageType where
     showConstantTermsSQL' = showConstantTermsSQL' . fromEnum
@@ -42,4 +30,3 @@ instance PersistField ImageType where
     toPersistValue = PersistInt64 . fromIntegral . fromEnum
     fromPersistValue (PersistInt64 v) = Right . toEnum . fromIntegral $ v
     fromPersistValue v = Left . T.pack $ "ImageType: Unkown Type, recieved: " ++ show v
-defineFromToSqlPersistValue [t|ImageType|]
