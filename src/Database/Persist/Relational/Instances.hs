744ec2373c72d3c69c78cfeeba039a3716ccd6f0
Takahiro HIMURA <taka@himura.jp>
2016-03-21 02:37:19 +0900

    wipe unused instances

--- src/Database/Persist/Relational/Instances.hs
+++ src/Database/Persist/Relational/Instances.hs
@@ -1,35 +1,11 @@
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Database.Persist.Relational.Instances
        where
 
-#if __GLASGOW_HASKELL__ < 710
-import Control.Applicative
-#endif
-import Database.Persist
 import Database.Persist.Relational.TH
-import Database.Record.FromSql
-import Database.Record.ToSql
-import Database.Relational.Query (ProductConstructor (..))
-
-instance PersistEntity record
-         => ProductConstructor (Key record -> record -> Entity record) where
-    productConstructor = Entity
-
-instance (ToSql PersistValue (Key record), ToSql PersistValue record)
-         => ToSql PersistValue (Entity record) where
-    recordToSql = wrapToSql $ \(Entity k v) -> do
-        putRecord k
-        putRecord v
-
-instance (PersistEntity record, FromSql PersistValue (Key record), FromSql PersistValue record)
-         => FromSql PersistValue (Entity record) where
-    recordFromSql = Entity <$> recordFromSql <*> recordFromSql
 
 derivePersistableInstancesFromPersistFieldInstances ["SomePersistField"]
